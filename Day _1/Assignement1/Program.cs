deecd2dd286915ac3e0311cb80bf446a96d94b40
deniel <denielforir@gmail.com>
2018-05-25 22:06:56 -0400

    homework 1 update

---
+++ Day _1/Assignement1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int myInt = 1;
+            Console.WriteLine("Alive");
+            if (myInt == 1)
+            {
+             Console.WriteLine("Alive");   
+            }
+            else if(myInt == 2)
+            {
+                Console.WriteLine("Dead");
+            }
+            else if (myInt == 3)
+            {
+                Console.WriteLine("Zombie")
+            }
+            else if (myInt == 4)
+            {Console.WriteLine("Unknown")}
+        }
+    }
+}
